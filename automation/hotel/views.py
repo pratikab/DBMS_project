2ee72a6f254ec17e389ce4df016d9f0803a44d1e
Pratik Bhangale <pratik@localhost.localdomain>
2018-04-06 04:27:15 +0530

    res change added

--- automation/hotel/views.py
+++ automation/hotel/views.py
@@ -9,7 +9,7 @@ import os
 from random import randint
 from hotel.models import *
 from datetime import datetime
-
+import copy
 from datetime import date
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
@@ -160,6 +160,7 @@ def reservation_form(request,username):
 	username = request.user.username
 	c = Customer.objects.get(username=username)
 	avail_rooms = RoomType.objects.all()
+	x = date.today().strftime('%Y-%m-%d')
 	error = ""
 	return render(
 		request,
@@ -167,7 +168,8 @@ def reservation_form(request,username):
 		{
 			'avail_rooms': avail_rooms,
 			'customer':c,
-			'error':error
+			'error':error,
+			'x' : x
 		}
 	)
 @login_required(login_url = '/login/')
@@ -289,15 +291,139 @@ def reservation_decide(request,username):
 def room_status(request,roomNo):
 	if request.user.isStaff:
 		room = Room.objects.get(room_no=roomNo)
-		print(room)
-		t = room.reservation_set.filter(expected_arrival_date__gte=date.today(),valid=True)
-		print(t)
+		t = room.reservation_set.filter(valid=True).exclude(expected_departure_date__lt=date.today())
 		return render(
 			request,
 			'room_status.html',
 			{
 				'roomNo': roomNo,
 				'res':t,	
+			}
+		)
+	return HttpResponse("Permission Denied.")
+
+@login_required(login_url = '/login/')
+def active_resr(request):
+	if request.user.isStaff:
+		t = Reservation.objects.filter(valid=True,expected_departure_date__gte=date.today(),expected_arrival_date__lte=date.today())
+		return render(
+			request,
+			'active_resr.html',
+			{
+				'res':t,	
+			}
+		)
+	return HttpResponse("Permission Denied.")
+
+@login_required(login_url = '/login/')
+def reservation_change(request):
+	if request.user.isStaff:
+		t = Reservation.objects.filter(valid=True).exclude(expected_departure_date__lt=date.today())
+		return render(
+			request,
+			'reservation_change.html',
+			{
+				'res':t,	
+			}
+		)
+	return HttpResponse("Permission Denied.")
+
+@login_required(login_url = '/login/')
+def res_view(request,res_id):
+	if request.user.isStaff:
+		t = [Reservation.objects.get(reservation_id=res_id)]
+		return render(
+			request,
+			'reservation_change.html',
+			{
+				'res':t,	
+			}
+		)
+	return HttpResponse("Permission Denied.")
+
+@login_required(login_url = '/login/')
+def res_del(request,res_id):
+	if request.user.isStaff and request.POST.get('action',False) == 'delete':
+		res = Reservation.objects.get(reservation_id=res_id)
+		res.delete()
+		message = "Deleted Successfully"
+		return render(
+			request,
+			'res_ack.html',
+			{
+				'message':message,	
+			}
+		)
+	return HttpResponse("Permission Denied.")
+
+@login_required(login_url = '/login/')
+def res_edit(request,res_id):
+	if request.user.isStaff and request.POST.get('action',False) == 'change':
+		res = Reservation.objects.get(reservation_id=res_id)
+		avail_rooms = RoomType.objects.all()
+		x = res.expected_arrival_date.strftime('%Y-%m-%d')
+		y = res.expected_departure_date.strftime('%Y-%m-%d')
+		return render(
+			request,
+			'change_form.html',
+			{
+				'res' : res,
+				'avail_rooms': avail_rooms,	
+				'x' : x,
+				'y' : y,
+			}
+		)
+	return HttpResponse("Permission Denied.")
+
+@login_required(login_url = '/login/')
+def res_ack(request,res_id):
+	if request.user.isStaff:
+		id_ = request.POST.get('id',False)
+		message = ""
+		res1 = Reservation.objects.get(reservation_id=id_)
+		res2 = copy.deepcopy(res1)
+		res1.delete()
+		res2.save()
+		username = request.POST.get('username',False)
+		c = Customer.objects.get(username=username)
+		roomtype = request.POST.get('room_select',False)
+		no_of_adults = request.POST.get('no_of_adults',False)
+		no_of_child = request.POST.get('no_of_child',False)
+		arrival_timestamp = request.POST.get('arrival_timestamp',False)
+		departure_timestamp = request.POST.get('departure_timestamp',False)
+		roomtype_obj = RoomType.objects.get(name=roomtype)
+		dummyroom = Room.objects.get(room_no='D-101')
+		rooms = roomtype_obj.room_set.all()
+		flag = False
+		for r in rooms:
+			t = r.reservation_set.filter(valid=True)
+			r1 = Reservation(reservation_id=0,customer=c,no_of_children=no_of_child,no_of_adults=no_of_adults,expected_arrival_date=arrival_timestamp,expected_departure_date=departure_timestamp,room_no=dummyroom,valid=False)
+			r1.save()
+			resr = Reservation.objects.get(reservation_id=0)
+			if not check_overlap(resr,t):
+				flag = True
+				room_allocated = r
+				resr.delete()
+				break
+			resr.delete()
+		if arrival_timestamp>departure_timestamp :
+			res2.save()
+			message = "Select departure date later than that of arrival date"
+		elif arrival_timestamp < str(date.today()) :
+			res2.save()
+			message = "Arrival date can't be in the past"
+		elif not flag:
+			res2.save()
+			message = "No rooms avaiable of this type for your dates"
+		else:
+			r1 = Reservation(reservation_id=res2.reservation_id,customer=res2.customer,no_of_children=no_of_child,no_of_adults=no_of_adults,expected_arrival_date=arrival_timestamp,expected_departure_date=departure_timestamp,room_no=room_allocated,valid=True)
+			r1.save()
+			message = "Reservation Changed Successfully"
+		return render(
+			request,
+			'res_ack.html',
+			{
+				'message':message
 			}
 		)
 	return HttpResponse("Permission Denied.")
--- automation/run.sh
+++ automation/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm db.sqlite3
-python manage.py migrate
-python manage.py populatedb
+#rm db.sqlite3
+#python manage.py migrate
+#python manage.py populatedb
 python manage.py runserver
